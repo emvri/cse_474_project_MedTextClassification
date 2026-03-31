cab2151514411da3c69a295d3a737ef4d6826602
Joseph Buczak <jbuczak789@gmail.com>
2026-03-30 23:02:00 -0400

    Added SVM to models.py, main.py and updated metrics.txt with values from SVM

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.svm import LinearSVC
 
 def train_nb(X_train, y_train):
     # Naive Bayes classifier
@@ -13,5 +13,11 @@ def train_nb(X_train, y_train):
 
 def train_lr(X_train, y_train):
     model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+    return model
+
+
+def train_svm(X_train, y_train):
+    model = LinearSVC(max_iter=1000)
     model.fit(X_train, y_train)
     return model
